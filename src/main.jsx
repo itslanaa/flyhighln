400f9a0a9aa99d8c8a7a15f0daa55feecd3ad3c6
Kaka Maulana Abdillah <maulanaabdillah73@gmail.com>
2025-12-19 21:39:29 +0700

    lg

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
-      <Routes>
-        <Route path='*' element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
